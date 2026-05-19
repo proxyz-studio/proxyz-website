c38d1b8e5d25450fb1e9d158a70853d101022333
Arnon Saksri <tew@portfolioplay.studio>
2026-05-19 10:38:49 +0700

    feat(i18n): mount LocaleProvider at app root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { LocaleProvider } from './i18n/LocaleContext'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <LocaleProvider>
+      <App />
+    </LocaleProvider>
   </BrowserRouter>
 )
